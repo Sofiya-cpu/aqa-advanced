16a6d10af1319b5a4d6c7bcfdfb5cdc18aa85c79
ahjop <hrytsyna.sph@gmail.com>
2024-08-27 01:24:40 +0300

    import chalk and enter some colorful text

---
+++ nodeBasics/index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk'; // імпорт бібліотеки
+
+//виводимо текст:
+console.log(chalk.green('The green text'));
+console.log(chalk.strikethrough.red('Some red text.'));
+console.log(chalk.bold.bgMagenta('What color is magenta?'));
