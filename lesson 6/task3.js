c9b3a7f7ccfc9c47b265e62c0b12819b8fb88428
ahjop <hrytsyna.sph@gmail.com>
2024-09-11 22:49:06 +0300

    edit the last version with one more possuble option_1

--- lesson 6/task3.js
+++ lesson 6/task3.js
@@ -2,14 +2,16 @@ function checkOrder(available, ordered) {
     if (available < ordered) {
         return "Your order is too large, we don’t have enough goods.";
     } else if (ordered == 0) {
-        return "Your order is empty";
+        return "Your order is empty.";
     } else if (available > ordered) {
         return "Your order is accepted.";
     }
 }
-/* or instead of the last 'else if' 
-} else {
+// or instead of the last 'else if': 
+/* } else {
     return "Your order is accepted";
 */
 
 console.log(checkOrder(100, 2));
+console.log(checkOrder(100, 0));
+console.log(checkOrder(100, 234));
